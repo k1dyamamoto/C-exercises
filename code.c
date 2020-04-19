4c266124aa738068a7d7c51beba68a4792b7d23a
wizard <41285485+k1dyamamoto@users.noreply.github.com>
2020-04-19 03:19:19 +0300

    Add files via upload

---
+++ code.c
@@ -0,0 +1,47 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int *partition(int *low, int *high) {
+	int p = *(low + (high - low) / 2);
+	while (low <= high) {
+		while (*low < p)
+			low++;
+		while (*high > p)
+			high--;
+		if (low >= high)
+			break;
+		swap(low++, high--);
+	}
+	return high;
+}
+
+void quicksort(int *low, int *high)
+{
+	if (low >= high)
+		return;
+	int *pivot = partition(low, high);
+	quicksort(low, pivot);
+	quicksort(pivot + 1, high);
+}
+
+
+main() {
+	int n;
+	scanf("%d\n", &n);
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	quicksort(arr, arr + n - 1);
+	for (int i = 0; i < n; i++) {
+		printf("%d ", *(arr + i));
+	}
+	printf("\n");
+}
